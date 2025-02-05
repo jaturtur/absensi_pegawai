dd32e37a0a79a3b2e282360827adb57fddd68b45
jaturtur <kotijahturahmah@mhs.politala.ac.id>
2025-02-05 13:42:55 +0800

    menambah simbol loogbook

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -46,3 +46,5 @@ $routes->post('pegawai/presensi_masuk_aksi', 'Pegawai\Home::presensi_masuk_aksi'
 
 $routes->post('pegawai/presensi_keluar/(:segment)', 'Pegawai\Home::presensi_keluar/$1', ['filter' => 'pegawaiFilter']);
 $routes->post('pegawai/presensi_keluar_aksi/(:segment)', 'Pegawai\Home::presensi_keluar_aksi/$1', ['filter' => 'pegawaiFilter']);
+
+$routes->get('pegawai/logbook', 'Pegawai\Logbook::index', ['filter' => 'pegawaiFilter']);
